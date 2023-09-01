6aa0a7949709a03ecdb4d524af3d36a5f5368e79
Austin Poor <45295232+a-poor@users.noreply.github.com>
2023-09-01 16:48:30 -0700

    Add images of my head

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,20 @@
 import Navbar from '$/components/Navbar';
+import AustinHead from '$/components/AustinHead';
 import { ActivePage } from "$/lib/dtypes";
 
 export default function Home() {
   return (
     <>
-      <header className="sticky top-0 left-0 right-0 backdrop-blur">
+      <header className="sticky top-0 left-0 right-0 backdrop-blur z-10">
         <Navbar 
           activePage={ActivePage.Home}
         />
       </header>
       <main className="">
-      {new Array(100).fill(0).map((_, i) => (
-        <p key={i}>Hello, World!</p>
-      ))}
+        <AustinHead />
+        {new Array(100).fill(0).map((_, i) => (
+          <p key={i}>Hello, World!</p>
+        ))}
       </main>
       <footer className=""></footer>
     </>
---
+++ components/AustinHead.tsx
@@ -0,0 +1,59 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import NextImage from 'next/image';
+
+const MS_PER_FRAME = 1000 / 10;
+const IMG_PATHS = new Array(18).fill(0)
+    .map((_, i) => `${i}`.padStart(2, '0'))
+    .map(n => `/images/austin-bntsg-${n}.png`);
+
+
+export default function AustinHead() {
+    const [imgPathIdx, setImgPathIdx] = useState(0);
+    useEffect(() => {
+        // Define the timer...
+        let timer: ReturnType<typeof setTimeout> | undefined = undefined;
+
+        // Load all of the images asynchronously...
+        Promise.all(IMG_PATHS.map(path => new Promise((resolve, reject) => {
+            const img = new Image();
+            img.onload = () => resolve(img);
+            img.onerror = reject;
+            img.src = path;
+        })))
+        .then(images => {
+            // Create the timer...
+            timer = setInterval(() => {
+                setImgPathIdx(idx => (idx + 1) % images.length);
+            }, MS_PER_FRAME);
+        });
+
+
+        // Callback to clear the timer...
+        return () => clearInterval(timer);
+    }, []);
+    return (
+        <div className="py-10 bg-emerald-400">
+            <div className="relative">
+                <NextImage 
+                    src={IMG_PATHS[imgPathIdx]} 
+                    alt="A photo of Austin's head, rotating in a circle, like in the Bill Nye the Science Guy intro."
+                    width={512}
+                    height={394}
+                    className="
+                        mx-auto
+                        w-[450px]
+                        rounded-full
+                    "
+                />
+                <div className="block absolute text-center -bottom-5 w-full">
+                    <h1 className="block text-center mx-auto text-6xl font-bold text-neutral-50 font-mono antialiased tracking-tighter">
+                        {/* Hi, I&apos;m Austin! */}
+                        Austin the Developer
+                    </h1>
+                </div>
+            </div>
+        </div>
+    );
+}
