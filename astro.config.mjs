e93f597b4d78fea00f63bd9f72a801a5b471a74f
Austin Poor <45295232+a-poor@users.noreply.github.com>
2024-11-08 07:23:09 -0800

    Install solid integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,14 @@ import sitemap from "@astrojs/sitemap";
 import cloudflare from "@astrojs/cloudflare";
 
 
+import solidJs from "@astrojs/solid-js";
+
+
 export default defineConfig({
   output: "hybrid",
   site: "https://austinpoor.com/",
   prefetch: true,
-  integrations: [
-    tailwind(),
-    sitemap(),
-  ],
+  integrations: [tailwind(), sitemap(), solidJs()],
   adapter: cloudflare(),
   image: {
     service: passthroughImageService(),
