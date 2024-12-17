2e6de939b9545a7ddf9539f1f6092ae9452d720d
Austin Poor <45295232+a-poor@users.noreply.github.com>
2024-12-16 21:56:16 -0800

    Updates app layout and adds theme picker

--- app/components/app-layout.tsx
+++ app/components/app-layout.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from 'react'
+import { Link } from 'react-router';
 import { Avatar } from '~/components/catalyst/avatar'
 import {
     Dropdown,
@@ -21,6 +22,7 @@ import {
     UserIcon,
 } from '@heroicons/react/16/solid'
 import { InboxIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid'
+import { Cpu } from '@phosphor-icons/react/dist/ssr'
 
 const navItems = [
     { label: 'Home', url: '/' },
@@ -58,64 +60,62 @@ function TeamDropdownMenu() {
 export function AppLayout({ children }: { children: ReactNode }) {
     return (
         <StackedLayout
-            navbar={
-                <Navbar>
-                    <Dropdown>
-                        <DropdownButton as={NavbarItem} className="max-lg:hidden">
-                            <Avatar src="/tailwind-logo.svg" />
-                            <NavbarLabel>Tailwind Labs</NavbarLabel>
-                            <ChevronDownIcon />
-                        </DropdownButton>
-                        <TeamDropdownMenu />
-                    </Dropdown>
-                    <NavbarDivider className="max-lg:hidden" />
-                    <NavbarSection className="max-lg:hidden">
-                        {navItems.map(({ label, url }) => (
-                            <NavbarItem key={label} href={url}>
-                                {label}
-                            </NavbarItem>
-                        ))}
-                    </NavbarSection>
-                    <NavbarSpacer />
-                    <NavbarSection>
-                        <NavbarItem href="/search" aria-label="Search">
-                            <MagnifyingGlassIcon />
-                        </NavbarItem>
-                        <NavbarItem href="/inbox" aria-label="Inbox">
-                            <InboxIcon />
-                        </NavbarItem>
-                        <Dropdown>
-                            <DropdownButton as={NavbarItem}>
-                                <Avatar src="/profile-photo.jpg" square />
-                            </DropdownButton>
-                            <DropdownMenu className="min-w-64" anchor="bottom end">
-                                <DropdownItem href="/my-profile">
-                                    <UserIcon />
-                                    <DropdownLabel>My profile</DropdownLabel>
-                                </DropdownItem>
-                                <DropdownItem href="/settings">
-                                    <Cog8ToothIcon />
-                                    <DropdownLabel>Settings</DropdownLabel>
-                                </DropdownItem>
-                                <DropdownDivider />
-                                <DropdownItem href="/privacy-policy">
-                                    <ShieldCheckIcon />
-                                    <DropdownLabel>Privacy policy</DropdownLabel>
-                                </DropdownItem>
-                                <DropdownItem href="/share-feedback">
-                                    <LightBulbIcon />
-                                    <DropdownLabel>Share feedback</DropdownLabel>
-                                </DropdownItem>
-                                <DropdownDivider />
-                                <DropdownItem href="/logout">
-                                    <ArrowRightStartOnRectangleIcon />
-                                    <DropdownLabel>Sign out</DropdownLabel>
-                                </DropdownItem>
-                            </DropdownMenu>
-                        </Dropdown>
-                    </NavbarSection>
-                </Navbar>
-            }
+          navbar={
+            <Navbar>
+              <NavbarItem as={Link} to="/" className="max-lg:hidden">
+                <Cpu size={28} />
+                <NavbarLabel>
+                  Austin Poor
+                </NavbarLabel>
+              </NavbarItem>
+              <NavbarDivider className="max-lg:hidden" />
+              <NavbarSection className="max-lg:hidden">
+                {navItems.map(({ label, url }) => (
+                  <NavbarItem key={label} href={url}>
+                    {label}
+                  </NavbarItem>
+                ))}
+              </NavbarSection>
+              <NavbarSpacer />
+              <NavbarSection>
+                  <NavbarItem href="/search" aria-label="Search">
+                      <MagnifyingGlassIcon />
+                  </NavbarItem>
+                  <NavbarItem href="/inbox" aria-label="Inbox">
+                      <InboxIcon />
+                  </NavbarItem>
+                  <Dropdown>
+                      <DropdownButton as={NavbarItem}>
+                          <Avatar src="/profile-photo.jpg" square />
+                      </DropdownButton>
+                      <DropdownMenu className="min-w-64" anchor="bottom end">
+                          <DropdownItem href="/my-profile">
+                              <UserIcon />
+                              <DropdownLabel>My profile</DropdownLabel>
+                          </DropdownItem>
+                          <DropdownItem href="/settings">
+                              <Cog8ToothIcon />
+                              <DropdownLabel>Settings</DropdownLabel>
+                          </DropdownItem>
+                          <DropdownDivider />
+                          <DropdownItem href="/privacy-policy">
+                              <ShieldCheckIcon />
+                              <DropdownLabel>Privacy policy</DropdownLabel>
+                          </DropdownItem>
+                          <DropdownItem href="/share-feedback">
+                              <LightBulbIcon />
+                              <DropdownLabel>Share feedback</DropdownLabel>
+                          </DropdownItem>
+                          <DropdownDivider />
+                          <DropdownItem href="/logout">
+                              <ArrowRightStartOnRectangleIcon />
+                              <DropdownLabel>Sign out</DropdownLabel>
+                          </DropdownItem>
+                      </DropdownMenu>
+                  </Dropdown>
+              </NavbarSection>
+            </Navbar>
+          }
             sidebar={
                 <Sidebar>
                     <SidebarHeader>
---
+++ app/components/theme-picker.tsx
@@ -0,0 +1,12 @@
+import {
+  Monitor,
+  SunDim,
+  Moon,
+} from '@phosphor-icons/react/dist/ssr';
+import {
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+} from '@headlessui/react';
+
