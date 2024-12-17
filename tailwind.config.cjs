16f9dd1d9bb6674284b134c40d5574e301dcd267
Austin Poor <45295232+a-poor@users.noreply.github.com>
2024-12-17 11:46:19 -0800

    Adds departure mono

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{html,js,jsx,md,mdx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        mono: ['DepartureMono', ...defaultTheme.fontFamily.mono],
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
