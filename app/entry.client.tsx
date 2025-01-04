09bc18c37d32ee7b9b467c4de177436d4928e4b0
Austin Poor <45295232+a-poor@users.noreply.github.com>
2025-01-04 10:20:47 -0800

    Fixes sentry config

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -4,7 +4,7 @@ import { HydratedRouter } from "react-router/dom";
 import * as Sentry from '@sentry/react';
 
 Sentry.init({
-  dsn: "https://8f2fd7a5991b5503f9c2e8e600c56a6f@o1081370.ingest.us.sentry.io/4508586546626560",
+  dsn: "https://f03fd02f0d0c93a039ebbb5e4a70c7c5@o1081370.ingest.us.sentry.io/4508586567794688",
   integrations: [
     Sentry.browserTracingIntegration(),
     Sentry.replayIntegration(),
