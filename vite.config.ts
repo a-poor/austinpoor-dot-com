e4c7a25ea5d08eba28d30f97716fb546281bc959
Austin Poor <45295232+a-poor@users.noreply.github.com>
2025-01-04 10:00:42 -0800

    Temporarily removes sentry source map push

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import yaml from 'js-yaml';
 import Markdoc from '@markdoc/markdoc';
 import readingTime from 'reading-time';
-import { sentryVitePlugin } from "@sentry/vite-plugin";
 
 function importBlogPostPlugin() {
   const virtualModuleId = 'virtual:load-blog-posts';
@@ -66,7 +65,6 @@ function importBlogPostPlugin() {
 
 export default defineConfig(({ isSsrBuild }) => ({
     build: {
-        sourcemap: true,
         rollupOptions: isSsrBuild
             ? {
                 input: "./workers/app.ts",
@@ -106,12 +104,5 @@ export default defineConfig(({ isSsrBuild }) => ({
         importBlogPostPlugin(),
         reactRouter(),
         tsconfigPaths(),
-
-        // Keep this at the end
-        sentryVitePlugin({
-          authToken: process.env.SENTRY_AUTH_TOKEN,
-          org: "austinpoorcom",
-          project: "austinpoor-dot-com",
-        }),
     ],
 }));
